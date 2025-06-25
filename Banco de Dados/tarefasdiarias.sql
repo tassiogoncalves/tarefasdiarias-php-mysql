72186085d75d744e28bab97456371baf08d34645
Tássio Gonçalves <tassiogoncalvesg@gmail.com>
2025-06-25 14:37:56 -0300

    Update tarefasdiarias.sql

--- Banco de Dados/tarefasdiarias.sql
+++ Banco de Dados/tarefasdiarias.sql
@@ -26,7 +26,7 @@ DROP TABLE IF EXISTS `categoria_tarefa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categoria_tarefa` (
-  `cod` int(11) NOT NULL,
+  `cod` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   PRIMARY KEY (`cod`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
